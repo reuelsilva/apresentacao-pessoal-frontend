741ee6a73efe6233f3b8cf48de7a4372a31e45c4
reuelsilva <reuelsilva19@gmail.com>
2024-10-08 17:29:59 -0300

    feat: configura o componente App para incluir Header, Main e Footer

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+function App(): JSX.Element {
+  return (
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App
