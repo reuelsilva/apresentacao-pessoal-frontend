f179f896e4a10326afd5686ad022c9da4d94efad
reuelsilva <reuelsilva19@gmail.com>
2024-10-08 17:28:16 -0300

    feat: adiciona o componente Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <footer className="bg-color-02 p-4 -mr-6 -ml-6 -mb-6 mt-6 sm:w-full sm:mr-0 sm:ml-0 sm:mb-0">
+            <p className="text-white text-center font-poppins-400">© 2024 Desenvolvedor Reuel Silva</p>
+        </footer>
+    )
+}
+
+export default Footer;
