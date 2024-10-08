fe81c130fc318fc5c72d3053881304949cb626c4
reuelsilva <reuelsilva19@gmail.com>
2024-10-08 20:19:15 -0300

    style:  altera o font-size do texto no footer de 16px para 12px

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     return(
         <footer className="bg-color-02 p-4 -mr-6 -ml-6 -mb-6 mt-6 sm:w-full sm:mr-0 sm:ml-0 sm:mb-0">
-            <p className="text-white text-center font-poppins-400">© 2024 Desenvolvedor Reuel Silva</p>
+            <p className="text-white text-xs text-center font-poppins-400">© 2024 Desenvolvedor Reuel Silva</p>
         </footer>
     )
 }
